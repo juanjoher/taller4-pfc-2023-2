6186eab879eff0adc649bf7859d13f143a83b453
Carlos-Camacho2003 <117177371+Carlos-Camacho2003@users.noreply.github.com>
2023-12-03 20:17:18 -0500

    Benchmark

--- app/src/test/scala/taller4/TestTaller4.scala
+++ app/src/test/scala/taller4/TestTaller4.scala
@@ -162,4 +162,22 @@ val obj = new Taller4()
         assert(335 == obj.prodPuntoParD(vector1, vector2))
     }
 
+    object Benchmark {
+        type Matriz = Vector[Vector[Int]]
+
+        def medirTiempo(funcion: => Unit): Double = {
+            val inicio = System.nanoTime()
+            funcion
+            val fin = System.nanoTime()
+            (fin - inicio) / 1e6 // Tiempo en milisegundos
+        }
+
+        def compararAlgoritmos(algoritmo1: (Matriz, Matriz) => Matriz, algoritmo2: (Matriz, Matriz) => Matriz)(m1: Matriz, m2: Matriz): (Double, Double, Double) = {
+            val tiempoAlgoritmo1 = medirTiempo(algoritmo1(m1, m2))
+            val tiempoAlgoritmo2 = medirTiempo(algoritmo2(m1, m2))
+            val relacionRendimiento = tiempoAlgoritmo1 / tiempoAlgoritmo2
+            (tiempoAlgoritmo1, tiempoAlgoritmo2, relacionRendimiento)
+        }
+    }
+
 }
